cf9847dafabeaace6245dd7472fdbf7f450e283b
Deepak <deepakvijayakumar143@gmail.com>
2024-06-15 22:40:26 +0530

    Revert "cleanup"
    This reverts commit 5e897ba420d90042ae3086949062be08ad2f0303.

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,62 @@ import ThirstyTree from "./ThirstyTree";
 
 // export default function App() {
 function App() {
+  // const [customTheme, setCustomTheme] = useState("light");
+  // const Theme = createTheme({
+  //   palette: {
+  //     primary: {
+  //       main: "#66B0B2",
+  //       light: "#AEE0E4",
+  //       // dark: will be calculated from palette.primary.main,
+  //       // contrastText: "#3e7674",
+  //     },
+  //     secondary: {
+  //       main: "#E0C2FF",
+  //       light: "#F5EBFF",
+  //       // dark: will be calculated from palette.secondary.main,
+  //       contrastText: "#47008F",
+  //     },
+  //     tertiary: {
+  //       main: "#3e7674",
+  //     },
+  //   },
+  // });
   return (
+    // <React.StrictMode>
+    // <ThemeProvider theme={Theme}>
+    //   <CssBaseline />
+    //   <HomePage
+    //     setCustomTheme={setCustomTheme}
+    //     customTheme={customTheme}
+    //     Theme={Theme}
+    //   ></HomePage>
+    // </ThemeProvider>
+    // </React.StrictMode>
     <>
       <h1>Welcome to the world of JSON ... a thirsty tree ...</h1>
       <Routes>
         // Add route here
+        {/* <switch> */}
         <Route path="/" element={<ThirstyTree />} />
         <Route path="/blog" element={<BuilderPage />} />
+        <Route path="/builder-demo" element={<BuilderPage />} />
         <Route path="/thirsty-tree" element={<ThirstyTree />} />
+        {/*   <Route
+            path="/"
+            component={
+              <React.StrictMode>
+                <ThemeProvider theme={Theme}>
+                  <CssBaseline />
+                  <HomePage
+                    setCustomTheme={setCustomTheme}
+                    customTheme={customTheme}
+                    Theme={Theme}
+                  ></HomePage>
+                </ThemeProvider>
+              </React.StrictMode>
+            }
+          />  */}
+        {/* </switch> */}
         // Add your other routes
       </Routes>
     </>
